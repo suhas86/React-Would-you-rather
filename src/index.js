b68f2ab301cbf10157c9e4d06c8f753583b03fee
suhas <murthy.suhas@gmail.com>
2018-05-25 22:44:15 +0530

    Persist data to handle page refresh

--- src/index.js
+++ src/index.js
@@ -9,8 +9,28 @@ import { Provider } from "react-redux";
 import reducer from "./reducers";
 import middleware from "./middleware";
 
-const store = createStore(reducer, middleware);
+function saveTOLocalStorage(state) {
+  try {
+    const serializeData = JSON.stringify(state);
+    localStorage.setItem("state", serializeData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+function loadFromLocalStorage() {
+  try {
+    const serializeData = localStorage.getItem("state");
+    if (serializeData === null) return undefined;
+    return JSON.parse(serializeData);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
+const persistedData = loadFromLocalStorage();
 
+const store = createStore(reducer,persistedData, middleware);
+store.subscribe(() => saveTOLocalStorage(store.getState()));
 ReactDOM.render(
   <Provider store={store}>
     <App />
